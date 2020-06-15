e72a84a4fadb7e3082a371d679a1c877594c4127
RodrigoLimaM <digo100200@gmail.com>
2020-06-15 18:41:50 -0300

    changing api model property

--- src/main/java/com/weather/model/WeatherDTO.java
+++ src/main/java/com/weather/model/WeatherDTO.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Builder
 public class WeatherDTO {
 
-    @ApiModelProperty(example = "celsius")
+    @ApiModelProperty(example = "CELSIUS")
     private TemperatureTypeEnum temperatureType;
 
     @ApiModelProperty(example = "31")
