69ec3d79906bfdde6adf87e0bffce0d1d1be5c2d
RodrigoLimaM <digo100200@gmail.com>
2020-07-13 01:00:06 -0300

    adding feign client log

--- src/main/java/com/weather/WeatherApplication.java
+++ src/main/java/com/weather/WeatherApplication.java
@@ -1,8 +1,11 @@
 package com.weather;
 
+import feign.Logger;
+import feign.Logger.Level;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -12,4 +15,8 @@ public class WeatherApplication {
 		SpringApplication.run(WeatherApplication.class, args);
 	}
 
+	@Bean
+	Level feignLoggerLevel() {
+		return Level.FULL;
+	}
 }
